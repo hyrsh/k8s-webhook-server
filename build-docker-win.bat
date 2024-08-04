fa6686de294e2aee05fbceff33ef8c2291802174
hyrsh <hiirrrsch@web.de>
2024-08-04 17:11:19 +0200

    changed image name to docker hub

--- build-docker-win.bat
+++ build-docker-win.bat
@@ -2,12 +2,12 @@
 SET CGO_ENABLED=0
 SET GOOS=linux
 SET GOARCH=amd64
-docker rmi webhooksrv:1.0
+docker rmi k8s-webhook-server:1.0
 go build -o webhooksrv_x86_64 -ldflags="-extldflags=-static -s -w" main.go
 timeout 2
 move ./webhooksrv_x86_64 ./docker/webhooksrv_x86_64
 chdir ./docker
-docker build -t webhooksrv:1.0 .
+docker build -t k8s-webhook-server:1.0 .
 timeout 2
 del "webhooksrv_x86_64"
 :: docker run --rm --name=liquidenv -v C:/Users/<USERNAME>/go/src/liquidenv/testarea/docker-configs:/cfg -d liquidenv:1.0
